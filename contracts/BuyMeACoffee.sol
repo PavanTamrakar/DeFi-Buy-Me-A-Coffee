a6f79363d47bdbe4dcd56b557eaa60706fdc93ff
PavanTamrakar <thepavantamrakar@gmail.com>
2023-02-02 16:30:45 +0530

    BMAC

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -1,3 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+contract BuyMeACoffee {
+
+    event NewMemo(
+        address indexed from,
+        uint timestamp,
+        string name,
+        string message
+    );
+
+    struct Memo{
+        address from;
+        uint timestamp;
+        string name;
+        string message;
+    }
+
+    address payable owner;
+
+    Memo[] memos;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function getMemo() public view returns (Memo[] memory) {
+        return memos;
+    }
+
+    function buyCoffee(string memory _name, string memory _message) public payable{
+        require(msg.value > 0, "Can't buy for free!");
+
+        
+    }
+}
