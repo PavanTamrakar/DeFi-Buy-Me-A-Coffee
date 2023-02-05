e43690d0e8b0bda7f25c1d1db5fc069828a31e02
PavanTamrakar <thepavantamrakar@gmail.com>
2023-02-05 19:39:31 +0530

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,21 +2,18 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config()
 
-const GOERLI_URL = process.env.GOERLI_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-/** @type import('hardhat/config').HardhatUserConfig */
-
-module.exports = {};
-
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
   networks: {
-    hardhat: {
-    },
     goerli: {
       url: GOERLI_URL,
-      account: [PRIVATE_KEY]
+      accounts: [PRIVATE_KEY]
     }
   }
 };
